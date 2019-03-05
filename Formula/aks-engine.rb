52187e468c938fa712fa1ad3729db89746b490cc
Matt Boersma <Matt.Boersma@microsoft.com>
2019-03-05 14:00:39 -0700

    feat: add aks-engine release v0.31.3
    See https://github.com/Azure/aks-engine/releases/tag/v0.31.3

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.31.2"
+    version "0.31.3"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "63ef3ea403a144bee86ca659db8fd40128d68f1ef4c722da1d9326de5571af87"
+    sha256 "955ac8a20b4152c1b4c697246dc6d55e62c1c34016b1d06e3eac6c44ba81511a"
 
     def install
       bin.install name
