37f8ec6af6783f7eb688faf7b657b63a36c8de8a
Jack Francis <jackfrancis@gmail.com>
2019-04-01 11:56:03 -0700

    feat: add v0.33.3 (#8)

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.33.2"
+    version "0.33.3"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "cc1d5443abe0d53c34d54c0b097899e40a8173c9e8ab20764a231cbd02d09d58"
+    sha256 "9ce3b35fc1395d4086cc47f86cc5b881936b00dbbba23ec40761215c943c3757"
 
     def install
       bin.install name
