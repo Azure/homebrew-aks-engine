1599cc2a87304070f42d1e7ba2403728df58c5ec
Matt Boersma <Matt.Boersma@microsoft.com>
2019-10-11 09:20:48 -0600

    chore: update aks-engine to v0.42.0 (#40)
    See https://github.com/Azure/aks-engine/releases/tag/v0.42.0

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.41.4"
+    version "0.42.0"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "692eed2d766c92903a603f832c4720e94cac2b3ca0165a12b4ef154ecf9d12ee"
+    sha256 "3544ce7d60177ac8c569502aad8b1a85d12a969480224a5f6e6219e1e636b1ab"
 
     def install
       bin.install name
