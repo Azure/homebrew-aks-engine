d0f3ea6bebb7c0b868c4a2a8b84c8a8e52fc6003
Salvatore Poliandro <sal@braze.com>
2020-01-22 02:47:42 +0000

    chore: update 0.46.0

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.45.0"
+    version "0.46.0"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "eb2468aa12cbffdcec10b4c65b379d5c39ea155aace65029e89d7e22e61bc866"
+    sha256 "3f31c18f5aa798143398eb22c448910d9553343c58d1244d2bc7f8a5a9df16b4"
 
     def install
       bin.install name
