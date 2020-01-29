c689fdbea4bc08bfae446df4ac5f01fe61bb603b
Jack Francis <jack.francis@microsoft.com>
2020-01-29 12:50:37 -0800

    chore: rev to v0.46.2

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.46.1"
+    version "0.46.2"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "a07197740c1775648abbf81d81c0afbade23cd70306f5afa57cd363fc8ed1acd"
+    sha256 "01bdfae8579e38e792a7df0eac9940b3a13aed4b898ddcb67a16f8bf5f24bb57"
 
     def install
       bin.install name
