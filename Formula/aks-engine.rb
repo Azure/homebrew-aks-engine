9b91f188f7d6be5d60f6aa19838cb0c48decbf9d
Matt Boersma <Matt.Boersma@microsoft.com>
2020-06-05 07:15:57 -0600

    chore: rev aks-engine to v0.52.0
    See https://github.com/Azure/aks-engine/releases/tag/v0.52.0

--- Formula/aks-engine.rb
+++ Formula/aks-engine.rb
@@ -4,9 +4,9 @@
 class AksEngine < Formula
     desc "A tool to provision Kubernetes clusters on Azure"
     homepage "https://github.com/Azure/aks-engine"
-    version "0.51.0"
+    version "0.52.0"
     url "https://github.com/Azure/aks-engine/releases/download/v#{version}/aks-engine-v#{version}-darwin-amd64.tar.gz"
-    sha256 "7424df1206f1318cc702cf5f6c12c6f0608ba4d68626473171caca6d879bca0e"
+    sha256 "a1ea61f0184066b17ee77b038d41de2d751ae7e18240c5221dd2fdec4fdd7695"
 
     def install
       bin.install name
